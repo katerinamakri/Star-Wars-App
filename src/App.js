857eac1cd9e8a6821ceddd72762e5917db546f13
katerinamakri <kathrin023@gmail.com>
2018-11-02 01:37:40 +0200

    Added functionality fetching the data

--- src/App.js
+++ src/App.js
@@ -9,16 +9,59 @@ class App extends Component {
     super(props)
 
     this.state = {
+      movieList: []
 
     }
   }
 
+  componentDidMount(){
+    this.fetchMovieData();
+  }
+
+  fetchMovieData = () => {
+
+    let apiUrl;
+
+    // this.setState({query: name});
+
+    apiUrl = 'https://star-wars-api.herokuapp.com/films'
+
+    fetch(apiUrl, { 
+      method: 'GET', 
+      headers: {
+        "Content-Type": "application/json"
+      } 
+    })
+    .then((response) => {
+
+      if (response.status === 404){
+        console.log ('No data')
+      } 
+      return response.json();
+    })
+    .then((results) => {
+       
+      this.setState({
+        movieList: results
+      })  
+
+    })
+    .catch((error) => {
+        // Code for handling errors
+        console.log(error)
+        alert("Sorry. There was an error retrieving the data. Please refer to the console for more information")
+    });    
+
+  }
+
   render() {
+
+    console.log(this.state.movieList)
     return (
       <div className="App">
         <Search />
         <div className="container">
-          <ListView />
+          <ListView movieList={this.state.movieList}/>
           <Movie />
         </div>        
       </div>
