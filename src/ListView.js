db478885d66cbe38a1f3c945a649c1fae8159355
katerinamakri <kathrin023@gmail.com>
2018-11-02 01:35:30 +0200

    Added movies in list

--- src/ListView.js
+++ src/ListView.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class ListView extends Component {
+
+// onClick={() => this.props.fetchCardData(card.cardName)}
 	render() {
 		return (
-			<div />
+			<div className="list-container">
+				<ul className="list" >
+					{this.props.movieList.map( (movie, index) => (
+						<li key={index} > 
+							<p> 
+								<span className='episode_id'>EPISODE {movie.fields.episode_id}</span>
+								<span className='movie-title'>{movie.fields.title}</span>
+								<span className='release_date'>{movie.fields.release_date}</span> 
+							</p>     				
+						</li> 
+					))}
+				</ul>
+			</div>
 		);
 	}
 }
