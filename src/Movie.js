21e8d84508b248d8c4f64d4beef43418cbaceaa6
katerinamakri <kathrin023@gmail.com>
2018-11-02 21:11:39 +0200

    Added functionality of Movie component

--- src/Movie.js
+++ src/Movie.js
@@ -3,9 +3,29 @@ import './App.css';
 
 class Movie extends Component {
 	render(){
-		return (
-			<div />
-		);
+
+		// this.props.query === '' && this.props.selectedCard.length === 0) ||
+		if ( (this.props.selectedMovie.fields === undefined))  {
+			return (
+				<div className="movie-container">
+					<div className="paragraph-container">
+						<p>No movie selected</p>
+					</div>
+				</div>
+			)
+		} else {		
+
+			const { title, opening_crawl, director } = this.props.selectedMovie.fields;
+			// console.log(this.props.selectedMovie.fields)
+			
+			return (
+				<div className="movie-container">
+						<h2> {title} </h2>
+						<p> {opening_crawl}</p>
+						<p> Directed by: <span> {director} </span></p>
+				</div>
+			);
+		}
 	}
 }
 
