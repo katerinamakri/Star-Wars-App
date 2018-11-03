3a8c664cabdfd47b09129e80a63a8dc76cb63f8f
katerinamakri <kathrin023@gmail.com>
2018-11-03 12:46:38 +0200

    Minor changes

--- src/Search.js
+++ src/Search.js
@@ -4,15 +4,13 @@ import './App.css';
 class Search extends Component {
 
 	render() {
-				      //  	value ={this.props.query}
-		       	// onChange={(event) => this.props.updateQuery(event.target.value)}
-
+	
 		return (
 			<div className="header-container"> 
 				<select>
-						<option value="volvo" defaultValue>Sort by...</option>
-					  <option value="volvo">Episode</option>
-  					<option value="saab">Year</option>
+						<option defaultValue>Sort by...</option>
+					  <option value="episode">Episode</option>
+  					<option value="year">Year</option>
 				</select>
 				<div className="search-movies-container">
 					<i className="fas fa-search"></i>
@@ -20,6 +18,8 @@ class Search extends Component {
 						className="search-input-movies"
 						type="text" 
 						placeholder="Type to search..."
+						value={this.props.query}
+		       	onChange={(event) => this.props.updateQuery(event.target.value)}
 					/>
 				</div>
 			</div>
