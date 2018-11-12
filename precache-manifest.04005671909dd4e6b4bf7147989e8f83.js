01b7b5a88dced6fc9fb873934fa87d8248a2703d
katerinamakri <kathrin023@gmail.com>
2018-11-12 23:55:47 +0200

    Updates

---
+++ precache-manifest.04005671909dd4e6b4bf7147989e8f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84868a484a137b772146",
+    "url": "/Star-Wars-App/static/js/runtime~main.84868a48.js"
+  },
+  {
+    "revision": "d874c22b672fa89e8660",
+    "url": "/Star-Wars-App/static/js/main.d874c22b.chunk.js"
+  },
+  {
+    "revision": "c7d5fa902d8b277fce9f",
+    "url": "/Star-Wars-App/static/js/1.c7d5fa90.chunk.js"
+  },
+  {
+    "revision": "d874c22b672fa89e8660",
+    "url": "/Star-Wars-App/static/css/main.91ed8d2a.chunk.css"
+  },
+  {
+    "revision": "6657835482ae7beef6d76ae465f55f6b",
+    "url": "/Star-Wars-App/index.html"
+  }
+];
